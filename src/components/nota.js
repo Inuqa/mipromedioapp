a9f50482cc5b8541cdce680596d2b70b9c8b2679
inuqa <seba.nrc@gmail.com>
2021-03-24 02:19:35 -0300

    builded again

--- src/components/nota.js
+++ src/components/nota.js
@@ -1,22 +1,23 @@
-import React from 'react'
+/* eslint react/prop-types: 0 */
+import React from 'react';
 
-const Nota = ({ value, index, onChange, onDestroy}) => {
-  const [grade, setGrade] = React.useState(0)
-  const [percentage, setPercentage] = React.useState(0)
+const Nota = ({value, index, onChange, onDestroy}) => {
+  const [grade, setGrade] = React.useState(0);
+  const [percentage, setPercentage] = React.useState(0);
 
-  const gradeChangeHandler = ({target:{value}}) => {
+  const gradeChangeHandler = ({target: {value}}) => {
     setGrade(+value);
     calculate(+value, percentage);
-  }
+  };
 
-  const percentageChangeHandler = ({ target:{value} }) => {
+  const percentageChangeHandler = ({target: {value}}) => {
     setPercentage(+value);
     calculate(grade, +value);
-  }
+  };
 
   const calculate = (_grade, _percentage) => {
     onChange(_grade * _percentage / 100, index);
-  }
+  };
 
   /*
   const onClickHandler = (e) => {
@@ -24,12 +25,12 @@ const Nota = ({ value, index, onChange, onDestroy}) => {
   }
   */
 
-  return(
+  return (
     <div className="nota">
       <p>Nota {index + 1}<input onChange={gradeChangeHandler} />
-      <input onChange={percentageChangeHandler} />%</p>
+        <input onChange={percentageChangeHandler} />%</p>
     </div>
   );
-}
+};
 
-export default Nota
+export default Nota;
